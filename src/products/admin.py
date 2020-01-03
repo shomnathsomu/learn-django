eebdbf0a338c170ce57fceef1de2362dd9c74fcc
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-04 04:53:50 +0600

    create python object in the python shell

--- src/products/admin.py
+++ src/products/admin.py
@@ -4,3 +4,9 @@ from django.contrib import admin
 from .models import Product
 
 admin.site.register(Product)
+
+
+# create python object in the python shell
+# python manage.py shell
+# from products.models import Product
+# Product.objects.create(title='Mercedes-Benz', description='Origin: Germany', price='$2M USD', summary='Looking pretty')
