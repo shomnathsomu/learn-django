e49f71672dc0f0d92a46136bdaa8c300e944f6ca
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-08 06:36:01 +0000

    Create Django Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,16 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-	print(args, kwargs)
-	print(request.user)
-	return HttpResponse("<h1>Hello World!</h1>") #string of HTML code
+	# print(args, kwargs)
+	# print(request.user)
+	# return HttpResponse("<h1>Hello World!</h1>") #string of HTML code
+	return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Contact us</h1>")
+	return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-	return HttpResponse("<h1>About us</h1>")
+	return render(request, "about.html", {})
 
 
 def social_view(request, *args, **kwargs):
