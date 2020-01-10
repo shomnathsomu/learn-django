a7d13fef943c7719c6b79a2f49220636de1d551a
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-10 09:25:40 +0000

    Using conditions in a template

--- src/pages/views.py
+++ src/pages/views.py
@@ -13,13 +13,13 @@ def contact_view(request, *args, **kwargs):
 
 
 def about_view(request, *args, **kwargs):
-        my_context = {
-	    "my_text": "This is about us",
-	    "my_number": 123,
-	    "my_array": [121,343,566,789,12,"dfg"]
+	my_context = {
+		"my_text": "This is about us",
+		"my_number": 123,
+		"my_array": [121,343,566,789,12,"dfg"]
 	}
-	return render(request, "about.html", my_context)
 
+	return render(request, "about.html", my_context)
 
 def social_view(request, *args, **kwargs):
 	return HttpResponse("<h1>Social page</h1>")
