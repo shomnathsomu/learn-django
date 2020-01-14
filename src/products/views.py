b2653f0abf8629127ae5bf7cc1d8d41ae1c410b6
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-14 12:08:40 +0600

    Create product definition

--- src/products/views.py
+++ src/products/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+    	form.save()
+    	form = ProductForm()
+
+    context = {
+    	'form': form
+    }
+
+    return render (request, "products/product_create.html", context)
+
+
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
     # context = {
