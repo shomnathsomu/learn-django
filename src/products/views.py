9e9549afd5b556e70ffaabe7024bb6957b8c2b88
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-16 16:11:49 +0600

    Render Raw Production Form

--- src/products/views.py
+++ src/products/views.py
@@ -1,29 +1,39 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductionForm
 
 # Create your views here.
 
-# Create for raw html form
 def product_create_view(request):
+    my_form = RawProductionForm()
     if request.method == "POST":
-        my_new_title = request.POST.get('title')
-        print(my_new_title)
-
-    context = {}
-
+        my_form = RawProductionForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = {
+        "form": my_form
+    }
     return render (request, "products/product_create.html", context)
+
+# Create for raw html form
+# def product_create_view(request):
+#     if request.method == "POST":
+#         my_new_title = request.POST.get('title')
+#         print(my_new_title)
+#     context = {}
+#     return render (request, "products/product_create.html", context)
 
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
 #     if form.is_valid():
 #     	form.save()
 #     	form = ProductForm()
-
 #     context = {
 #     	'form': form
 #     }
-
 #     return render (request, "products/product_create.html", context)
 
 
