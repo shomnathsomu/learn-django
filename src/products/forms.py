465c4ac17107976d970294b175b183c8f35e4afd
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-16 16:05:06 +0600

    Create form widgets

--- src/products/forms.py
+++ src/products/forms.py
@@ -11,3 +11,27 @@ class ProductForm(forms.ModelForm):
 			'description',
 			'price'
 		]
+
+
+class RawProductionForm(forms.Form):
+	title       = forms.CharField(
+					label='',
+					widget=forms.TextInput(
+							attrs={
+								"placeholder": "Your Title"
+							}
+						)
+					)
+	description = forms.CharField(
+					required=False,
+					widget=forms.Textarea(
+							attrs={
+								"placeholder": "Your description",
+								"class": "new-class-name two",
+								"id": "my-id-for-textarea",
+								"rows": 20,
+								"cols": 120
+							}
+						)
+					)
+	price 		= forms.DecimalField(initial=199.99)
