fbff3615ffc721db0c2993a9cb5a16cecae64c21
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-17 10:16:04 +0600

    Check ProductForm validation

--- src/products/views.py
+++ src/products/views.py
@@ -4,19 +4,19 @@ from .forms import ProductForm, RawProductionForm
 
 # Create your views here.
 
-def product_create_view(request):
-    my_form = RawProductionForm()
-    if request.method == "POST":
-        my_form = RawProductionForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {
-        "form": my_form
-    }
-    return render (request, "products/product_create.html", context)
+# def product_create_view(request):
+#     my_form = RawProductionForm()
+#     if request.method == "POST":
+#         my_form = RawProductionForm(request.POST)
+#         if my_form.is_valid():
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         "form": my_form
+#     }
+#     return render (request, "products/product_create.html", context)
 
 # Create for raw html form
 # def product_create_view(request):
@@ -26,15 +26,15 @@ def product_create_view(request):
 #     context = {}
 #     return render (request, "products/product_create.html", context)
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#     	form.save()
-#     	form = ProductForm()
-#     context = {
-#     	'form': form
-#     }
-#     return render (request, "products/product_create.html", context)
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+    	form.save()
+    	form = ProductForm()
+    context = {
+    	'form': form
+    }
+    return render (request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
