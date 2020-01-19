94b6ea9498e684292e353e9ffb89c36f8f4965c0
Shomnath Pramanik <shomnathcse22@gmail.com>
2020-01-19 19:42:11 +0600

    Create initial values for forms

--- src/products/views.py
+++ src/products/views.py
@@ -4,6 +4,22 @@ from .forms import ProductForm, RawProductionForm
 
 # Create your views here.
 
+def render_intial_data(request):
+    initial_data = {
+        'title': "My awesome title"
+    }
+
+    obj = Product.objects.get(id=1)
+    form = ProductForm(request.POST or None, instance=obj)
+
+    if form.is_valid():
+        form.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
 # def product_create_view(request):
 #     my_form = RawProductionForm()
 #     if request.method == "POST":
